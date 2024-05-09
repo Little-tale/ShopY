ca05044f1b64c1810decaa25f2f0e4d72422e853
judge_happily <you3192001@gmail.com>
2024-05-10 00:17:41 +0900

    Fetch: gitIgnore

--- NaverShopping/Manager/Network/NetworkManager.swift
+++ NaverShopping/Manager/Network/NetworkManager.swift
@@ -8,50 +8,9 @@
 import Foundation
 import Combine
 
-protocol TargetType {
+
+struct NetworkManager {
     
-    var baseUrl: String { get }
+    //typealias FetchType<T: Decodable> = AnyPublisher
     
-    var path: String { get }
-    
-    var method: HTTPMethod { get }
-    
-    var parametter: [ String: Any ]? { get }
-    
-    var queryItems: [URLQueryItem]? { get }
-    
-    
-// 시간상 생략
-//    func errorCase(_ errorCode: Int, _ description: String) -> NetworkError
 }
-
-extension TargetType {
-    
-    func asURLRequest() -> Result<URLRequest, NetworkError> {
-        
-        let baseURL = URL(string: baseUrl)
-        
-        guard let baseURL else {
-            return .failure(.errorResponse)
-        }
-        
-        let url = baseURL.appendingPathComponent(path, conformingTo: .url)
-        
-        var urlCompponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
-        
-        urlCompponents?.queryItems = queryItems
-        
-        guard let lastURL = urlCompponents?.url else {
-            return .failure(.errorResponse)
-        }
-        
-        var urlRequest = URLRequest(url: lastURL)
-        print(method.rawValue)
-        urlRequest.httpMethod = method.rawValue
-        return .success(urlRequest)
-        
-    }
-}
-
-
-
---
+++ NaverShopping/Protocol/TargetType.swift
@@ -0,0 +1,56 @@
+//
+//  TargetType.swift
+//  NaverShopping
+//
+//  Created by Jae hyung Kim on 5/10/24.
+//
+
+import Foundation
+
+protocol TargetType {
+    
+    var baseUrl: String { get }
+    
+    var path: String { get }
+    
+    var method: HTTPMethod { get }
+    
+    var parametter: [ String: Any ]? { get }
+    
+    var queryItems: [URLQueryItem]? { get }
+    
+    
+// 시간상 생략
+//    func errorCase(_ errorCode: Int, _ description: String) -> NetworkError
+}
+
+extension TargetType {
+    
+    func asURLRequest() -> Result<URLRequest, NetworkError> {
+        
+        let baseURL = URL(string: baseUrl)
+        
+        guard let baseURL else {
+            return .failure(.errorResponse)
+        }
+        
+        let url = baseURL.appendingPathComponent(path, conformingTo: .url)
+        
+        var urlCompponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        
+        urlCompponents?.queryItems = queryItems
+        
+        guard let lastURL = urlCompponents?.url else {
+            return .failure(.errorResponse)
+        }
+        
+        var urlRequest = URLRequest(url: lastURL)
+        print(method.rawValue)
+        urlRequest.httpMethod = method.rawValue
+        return .success(urlRequest)
+        
+    }
+}
+
+
+
