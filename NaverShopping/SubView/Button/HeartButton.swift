8768b8d78eacaaf2a19f769febd23f2879c9bccb
judge_happily <you3192001@gmail.com>
2024-05-09 18:16:32 +0900

    Fetch: 하트버튼

---
+++ NaverShopping/SubView/Button/HeartButton.swift
@@ -0,0 +1,31 @@
+//
+//  HeartButton.swift
+//  NaverShopping
+//
+//  Created by Jae hyung Kim on 5/9/24.
+//
+
+import SwiftUI
+
+struct HeartButton: View {
+    
+    @Binding
+    var isSelected: Bool
+    let tag: Int
+    let action: () -> Void
+    
+    var body: some View {
+        
+        Button (action: action) {
+            Image(systemName: isSelected ? "heart.fill" : "heart" )
+                .foregroundStyle(.red)
+        }
+        .padding(.all, 8)
+        .background(
+            .white
+        )
+        .clipShape(Circle(), style: FillStyle())
+       
+        .tag(tag)
+    }
+}
