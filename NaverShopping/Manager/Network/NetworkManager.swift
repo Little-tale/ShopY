ed35b7bce24c2fc048a1098fcb8ef1280a062cb4
judge_happily <you3192001@gmail.com>
2024-05-10 00:55:38 +0900

    Fetch: 네트워크 메니저 구성 (테스트 전)

--- NaverShopping/Manager/Network/NetworkManager.swift
+++ NaverShopping/Manager/Network/NetworkManager.swift
@@ -11,6 +11,39 @@ import Combine
 
 struct NetworkManager {
     
-    //typealias FetchType<T: Decodable> = AnyPublisher
+    typealias FetchType<T: Decodable> = AnyPublisher<T,NetworkError>
+    
+}
+
+extension NetworkManager {
+    
+    static func fetchNetwork<T:Decodable>(model: T.Type, router: NaverRouter) -> FetchType<T> {
+        Future<T, NetworkError> { promiss in
+            Task {
+                do {
+                    let urlRequest =  router.asURLRequest()
+                    guard case .success(let success) = urlRequest else {
+                        return promiss(.failure(NetworkError.failRequest))
+                    }
+                    let (data, response) = try await URLSession.shared.data(for: success)
+                    
+                    guard let response = response as? HTTPURLResponse else {
+                        return promiss(.failure(.noResponse))
+                    }
+                    
+                    if !(200..<300).contains(response.statusCode) {
+                        promiss(.failure(.cantStatusCoding))
+                    }
+                    
+                    let decoding = try JSONDecoder().decode(T.self, from: data)
+                    promiss(.success(decoding))
+                    
+                } catch {
+                    promiss(.failure(.unknownError))
+                }
+            }
+        }.eraseToAnyPublisher()
+        
+    }
     
 }
