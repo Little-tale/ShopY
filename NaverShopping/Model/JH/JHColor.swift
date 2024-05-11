e3cc2664bfd00a66e1c1d1308bbd1f944413082e
judge_happily <you3192001@gmail.com>
2024-05-11 14:08:07 +0900

    Fetch: 컬러 셋팅

---
+++ NaverShopping/Model/JH/JHColor.swift
@@ -0,0 +1,27 @@
+//
+//  JHColor.swift
+//  NaverShopping
+//
+//  Created by Jae hyung Kim on 5/11/24.
+//
+
+import SwiftUI
+
+enum JHColor {
+}
+
+extension JHColor {
+    
+    static let black: Color = .textBlack
+    static let white: Color = .textWhite
+    static let onlyWhite: Color = .onlyWhite
+    static let pointGreen: Color = .pointGreen
+    static let gray: Color = .backGray
+    static let darkGray: Color = .backDarkGray
+    static let likeColor: Color = .like
+    static let settingColor: Color = .setting
+    static let warningColor: Color = .warning
+    static let onlyBlack: Color = .onlyBlack
+    static let onlyDarkGray: Color = .darkGrayOnly
+    static let segementBlack: Color = .segmentBlack
+}
