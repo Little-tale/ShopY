46ff5804313544da2d5ad18822acaa9ff5603ec9
judge_happily <you3192001@gmail.com>
2024-05-12 14:35:26 +0900

    fetch: 기본적인 루트 정하기

--- NaverShopping/View/UserInfoReg/UserInfoRegView.swift
+++ NaverShopping/View/UserInfoReg/UserInfoRegView.swift
@@ -10,15 +10,59 @@ import PhotosUI
 
 struct UserInfoRegView: View {
     
+    @State
+    var selectedImage: UIImage? = nil
+    
     var body: some View {
         VStack {
-            PhotoView()
-            
+            Text("프로필 정하기")
+                .font(.title)
+                .bold()
+            HStack {
+                if let selectedImage {
+                    Image(uiImage: selectedImage)
+                        .resizable()
+                        .modifier(UserImageViewModifier())
+                } else {
+                    Image(systemName: "person")
+                        .resizable()
+                        .modifier(UserImageViewModifier())
+                }
+            }
+            .padding(.horizontal, 140)
+            .asButton {
+                print("이때 이미지 수정 알렛")
+            }
+            .buttonStyle(UserProfileImageButtonStyle())
+            Spacer()
         }
         
     }
 }
 
+struct UserImageViewModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .aspectRatio(1, contentMode: .fit)
+            .frame(maxWidth: .infinity)
+            .clipShape(Circle(), style: FillStyle())
+            .overlay(
+                Circle()
+                    .stroke(Color.black, lineWidth: 2)
+            )
+    }
+}
+
+struct UserProfileImageButtonStyle: ButtonStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .tint(.black)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1)
+    }
+}
+
 #Preview {
-    SplashView()
+    UserInfoRegView()
 }
