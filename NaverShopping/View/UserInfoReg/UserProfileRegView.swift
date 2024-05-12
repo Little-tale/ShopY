43586294c9588801b3c28fe09a553dfa83e32a1a
judge_happily <you3192001@gmail.com>
2024-05-12 20:51:31 +0900

    Fetch: 프로필 이미지 뷰를 정의합니다.

--- NaverShopping/View/UserInfoReg/UserProfileRegView.swift
+++ NaverShopping/View/UserInfoReg/UserProfileRegView.swift
@@ -8,3 +8,25 @@
 import SwiftUI
 import PhotosUI
 
+
+struct ProfileImage: View {
+    
+    let imageState: ProfileViewModel.ImageState
+    
+    var body: some View {
+        switch imageState {
+        case .empty:
+            Image(systemName: "person.fill")
+                .font(.system(size: 40))
+                .foregroundStyle(.white)
+        case .loading:
+            ProgressView()
+        case .success(let image):
+            image.resizable()
+        case .failure(let error):
+            Image(systemName: "exclamationmark.triangle.fill")
+                .font(.system(size: 40))
+                .foregroundStyle(.white)
+        }
+    }
+}
