94401a013ea3f70a4a2b4922793378e83d308abc
judge_happily <you3192001@gmail.com>
2024-05-15 01:00:58 +0900

    Fetch: 기본적 Realm 레포지토리

---
+++ NaverShopping/Manager/Realm/RealmError.swift
@@ -0,0 +1,14 @@
+//
+//  RealmError.swift
+//  NaverShopping
+//
+//  Created by Jae hyung Kim on 5/15/24.
+//
+
+import Foundation
+
+enum RealmError: Error {
+    case cantLoadRealm
+    case failAdd
+    case failRemove
+}
