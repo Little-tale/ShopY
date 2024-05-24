a11054a1b7e0d6e56ceff6e94b591b249c1003a6
Kim JaeHyung <you3192001@icloud.com>
2024-05-24 15:02:32 +0900

    Update: networkError, DTOType

--- ShopY/Manager/Network/NetworkError.swift
+++ ShopY/Manager/Network/NetworkError.swift
@@ -20,11 +20,25 @@ enum NetworkError: Error {
 
 extension NetworkError {
     
-    
+    // 임시로 에러를 정의합니다
     var errorMessgae: String {
         switch self {
-        default:
-            return "에러가 나따"
+        case .nodata:
+            return "데이터 에러"
+        case .noResponse:
+            return "응답 에러 무"
+        case .errorResponse:
+            return "데이터 에러"
+        case .failRequest:
+            return "요청 실패"
+        case .errorDecoding:
+            return "디코딩 에러"
+        case .cantStatusCoding:
+            return "상태코드 실패"
+        case .statusErrorCode:
+            return "상태 코드 에러 (임시)"
+        case .unknownError:
+            return "알수없음"
         }
     }
 }
--- ShopY/Protocol/DTOType.swift
+++ ShopY/Protocol/DTOType.swift
@@ -7,4 +7,4 @@
 
 import Foundation
 
-protocol DTOType { }
+protocol DTOType: Decodable { }
