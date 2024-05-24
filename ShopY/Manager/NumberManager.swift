997284b27d7c5a00cc0890d8809a2eb5c70a648e
Kim JaeHyung <you3192001@icloud.com>
2024-05-24 15:03:40 +0900

    Fetch: NumberManager

--- ShopY/Manager/NumberManager.swift
+++ ShopY/Manager/NumberManager.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct NumberManager {
+    private let formatter = NumberFormatter()
+    
+    enum UnitCase: String {
+        case money = " 원"
+    }
+    
+    static let shared = NumberManager()
+    
+}
+
+extension NumberManager {
+    
+    func getTextToMoney(text: String) -> String {
+        formatter.numberStyle = .decimal
+        
+        let numberString = formatter.string(for: Int(text))
+        
+        if let numberString {
+            return numberString + UnitCase.money.rawValue
+        } else {
+            return UnitCase.money.rawValue
+        }
+    }
+}
