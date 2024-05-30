98b0ed65aa3fea6ec79742dcfaa60ca6982e3af4
Kim JaeHyung <you3192001@icloud.com>
2024-05-30 21:25:35 +0900

    Update: 네트워크 로직을 분리하였습니다.

--- ShopY/Manager/Network/NetworkManager.swift
+++ ShopY/Manager/Network/NetworkManager.swift
@@ -9,49 +9,84 @@ import Foundation
 import Combine
 
 
-struct NetworkManager {
+protocol NetworkManagerType {
     
     typealias FetchType<T: Decodable> = AnyPublisher<T,NetworkError>
     
+    static func fetchNetwork<T:Decodable>(model: T.Type, router: NaverRouter) -> FetchType<T>
+    
+    static func checkReqeust<T: Decodable>(type: T.Type, router: NaverRouter) async throws -> T
+    
+    static func checkURLRequest(router: NaverRouter) throws -> URLRequest
+    
+    static func checkURLResponse(response: URLResponse) throws
+    
+    static func decode<T: Decodable>(data: Data) throws -> T
 }
+
+struct NetworkManager: NetworkManagerType { }
 
 extension NetworkManager {
     
     static func fetchNetwork<T:Decodable>(model: T.Type, router: NaverRouter) -> FetchType<T> {
+        
         Future<T, NetworkError> { promiss in
             Task {
                 do {
-                    let urlRequest =  router.asURLRequest()
-                    
-                    guard case .success(let success) = urlRequest else {
-                        return promiss(.failure(NetworkError.failRequest))
-                    }
-                         
-                    let (data, response) = try await URLSession.shared.data(for: success)
-                    
-                    guard let response = response as? HTTPURLResponse else {
-                        print( "Response Error ")
-                        return promiss(.failure(.noResponse))
-                    }
-                    
-                    if !(200..<300).contains(response.statusCode) {
-                        return promiss(.failure(.cantStatusCoding))
-                    }
-                    do {
-                        let decoding = try JSONDecoder().decode(T.self, from: data)
-                        promiss(.success(decoding))
-                    } catch {
-                        print( "errorDecoding Error ")
-                        promiss(.failure(.errorDecoding))
-                    }
+                    let result = try await checkReqeust(type: model, router: router)
+                    promiss(.success(result))
+                } catch let error as NetworkError {
+                    promiss(.failure(error))
                 } catch {
-                    print( "unknownError Error ")
                     promiss(.failure(.unknownError))
                 }
             }
-        }.eraseToAnyPublisher()
-        
+        }
+        .eraseToAnyPublisher()
     }
     
+    static func checkReqeust<T: Decodable>(type: T.Type, router: NaverRouter) async throws -> T {
+        
+        let urlRequest = try checkURLRequest(router: router)
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        try checkURLResponse(response: response)
+        
+        return try decode(data: data)
+    }
+    
+    static func checkURLRequest(router: NaverRouter) throws -> URLRequest {
+        
+        let urlRequest =  router.asURLRequest()
+        
+        guard case .success(let success) = urlRequest else {
+            throw NetworkError.failRequest
+        }
+        
+        return success
+    }
+    
+    // CHECK  URLResponse
+    static func checkURLResponse(response: URLResponse) throws {
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.noResponse
+        }
+        
+        guard (200..<300).contains(httpResponse.statusCode) else {
+            throw NetworkError.cantStatusCoding
+        }
+    }
+    
+    // Decode Method
+    static func decode<T: Decodable>(data: Data) throws -> T {
+        do {
+            let decoding = try JSONDecoder().decode(T.self, from: data)
+            return decoding
+        } catch {
+            print( "errorDecoding Error ")
+            throw NetworkError.errorDecoding
+        }
+    }
 }
 
